b4c8afab151e7320f17212c3adcfaaf1816aec34
Wolfgang Ramos <wolfgang@ramosarhuis.de>
2019-02-28 01:34:58 +0100

    added viztab-pkg.el file needed for melpa

---
+++ viztab-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "viztab" "20190228.2245" "Format list or vector structures as tables." '((emacs "25.1") (cl-generic "1.0")) :url "https://github.com/WolfgangRamos/viztab")
